656656f62245893e4dbe24411b08cfce6833fef6
feligones <felipe.gonzalez@factored.ai>
2022-12-15 13:00:34 -0500

    [WIP] Data extraction and EDA

---
+++ conf/.ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,35 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.signal import periodogram
+
+
+def plot_periodogram(ts, detrend='linear', ax=None):
+    
+    fs = pd.Timedelta("1Y") / pd.Timedelta("1M")
+    freqencies, spectrum = periodogram(
+        ts,
+        fs=fs,
+        detrend=detrend,
+        window="boxcar",
+        scaling='spectrum',
+    )
+    if ax is None:
+        _, ax = plt.subplots()
+    ax.step(freqencies, spectrum, color="purple")
+    ax.set_xscale("log")
+    ax.set_xticks([1, 2, 4, 6, 12])
+    ax.set_xticklabels(
+        [
+            "Annual (1)",
+            "Semiannual (2)",
+            "Quarterly (4)",
+            "Bimonthly (6)",
+            "Monthly (12)"
+        ],
+        rotation=30,
+    )
+    ax.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+    ax.set_ylabel("Variance")
+    ax.set_title("Periodogram")
+    return ax
