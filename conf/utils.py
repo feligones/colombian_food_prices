6befd84c80b9366c579451bdcb5f8feda74a27ea
feligones <felipe.gonzalez@factored.ai>
2022-12-16 16:34:23 -0500

    [ADD] prediction POC

--- conf/utils.py
+++ conf/utils.py
@@ -1,35 +1,16 @@
 
-import matplotlib.pyplot as plt
 import pandas as pd
-from scipy.signal import periodogram
 
+def make_lags(ts, lags):
+    return pd.concat(
+        {
+            f'y_lag_{i}': ts.shift(i)
+            for i in range(1, lags + 1)
+        },
+        axis=1)
 
-def plot_periodogram(ts, detrend='linear', ax=None):
-    
-    fs = pd.Timedelta("1Y") / pd.Timedelta("1M")
-    freqencies, spectrum = periodogram(
-        ts,
-        fs=fs,
-        detrend=detrend,
-        window="boxcar",
-        scaling='spectrum',
-    )
-    if ax is None:
-        _, ax = plt.subplots()
-    ax.step(freqencies, spectrum, color="purple")
-    ax.set_xscale("log")
-    ax.set_xticks([1, 2, 4, 6, 12])
-    ax.set_xticklabels(
-        [
-            "Annual (1)",
-            "Semiannual (2)",
-            "Quarterly (4)",
-            "Bimonthly (6)",
-            "Monthly (12)"
-        ],
-        rotation=30,
-    )
-    ax.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
-    ax.set_ylabel("Variance")
-    ax.set_title("Periodogram")
-    return ax
+def make_multistep_target(ts, steps):
+    return pd.concat(
+        {f'y_step_{i + 1}': ts.shift(-i)
+         for i in range(steps)},
+        axis=1)
