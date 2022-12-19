a91825ec7c1a9004563d32d9b5a6ff89b9999296
feligones <felipe.gonzalez@factored.ai>
2022-12-19 16:42:02 -0500

    [WIP] fix notebook stetics and save aux defs

--- conf/utils.py
+++ conf/utils.py
@@ -1,5 +1,9 @@
 
 import pandas as pd
+import numpy as np
+from statsmodels.tsa.seasonal import seasonal_decompose, STL
+from statsmodels.tsa.deterministic import DeterministicProcess
+
 
 def make_lags(ts, lags):
     return pd.concat(
@@ -25,6 +29,57 @@ def make_lags_features(ts, lags):
         }
         ,
         axis=1)
+
+def get_X_det(y, forecast_steps = 1):
+    res_robust = STL(y, period=12, robust=True).fit()
+    f_t = max(0, 1 - res_robust.resid.var()/(res_robust.trend + res_robust.resid).var())
+    f_s = max(0, 1 - res_robust.resid.var()/(res_robust.seasonal + res_robust.resid).var())
+    
+    dp = DeterministicProcess(
+        index = y.index,
+        constant = True,
+        order = (2 if f_t > 0.5 else 0),
+        drop = True,
+        seasonal = (True if f_s > 0.5 else False)
+    )
+    
+    X = dp.in_sample()
+    X_fore = dp.out_of_sample(steps = forecast_steps)
+    
+    return X, X_fore
+
+def get_model(y, X, model, return_residuals):
+    model.fit(X, y)
+    y_pred = pd.Series(model.predict(X), index = y.index)
+    if return_residuals:
+        residuals = y - y_pred
+        return model, y_pred, residuals
+    else:
+        return model, y_pred
+
+def get_X_res(residuals, lags):
+    res_lags = make_lags_features(residuals, lags).dropna()
+    res_lags_last = res_lags[-1:]
+    res_lags_last.index = res_lags_last.index.shift(1)
+    residuals_ri, res_lags = residuals.align(res_lags, join='inner', axis=0)
+    return residuals_ri, res_lags_last, res_lags
+
+def get_prediction(models, Xs, res_std):
+    model_1 = models[0]
+    model_2 = models[1]
+    
+    X_1 = Xs[0]
+    X_2 = Xs[1]
+    
+    point_pred = model_1.predict(X_1)[0] + model_2.predict(X_2)[0]
+    pred = {
+        'point': point_pred,
+        'lower': point_pred - res_std,
+        'upper': point_pred + res_std,
+        'index': X_1.index[0]
+    }
+    
+    return pred
 
 def make_multistep_target(ts, steps):
     return pd.concat(
