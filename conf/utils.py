3e289b432615e5436a51a1183fd9898152300ea2
feligones <felipe.gonzalez@factored.ai>
2022-12-19 16:22:51 -0500

    [WIP] finish train and pred defs

--- conf/utils.py
+++ conf/utils.py
@@ -9,6 +9,23 @@ def make_lags(ts, lags):
         },
         axis=1)
 
+def make_lags_features(ts, lags):
+    simple_features = {
+        f'y_lag_{i}': ts.shift(i)
+        for i in range(1, lags + 1)
+    }
+    rolling_features = {
+        f'y_rolling_std': ts.shift(1).rolling(lags).std()
+    }
+    
+    return pd.concat(
+        {
+            **simple_features,
+            **rolling_features
+        }
+        ,
+        axis=1)
+
 def make_multistep_target(ts, steps):
     return pd.concat(
         {f'y_step_{i + 1}': ts.shift(-i)
